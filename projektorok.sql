1659d61e819c303dba8638b1e36aec01b4500536
hubinab <hubinab@gmail.com>
2025-02-18 13:43:18 +0100

    sql2

--- projektorok.sql
+++ projektorok.sql
@@ -29,12 +29,16 @@ SELECT DISTINCT `nev` FROM `projektorok` where `projektor` in (1,3,4) ORDER BY `
 SELECT COALESCE(`nev`,'ismeretlen'), COALESCE(`terem`, 'ismeretlen') FROM `projektorok` where `elvitte` is not null and `visszahozta` is null
 
 -- 13. feladat
-
+SELECT `terem`, `elvitte` FROM `projektorok` order by `elvitte` desc limit 1
 
 -- 14. feladat
+SELECT `nev` FROM `projektorok` WHERE `terem` between 15 and 25 
 
 -- 15. feladat
+SELECT `nev` FROM `projektorok` WHERE `terem` BETWEEN 100 and 199 and `hangszoro` is not null
 
 -- 16. feladat
+SELECT `projektor`, `elvitte` FROM `projektorok` WHERE `nev` is null and `visszahozta` is null
 
 -- 17. feladat
+SELECT `nev`, `visszahozta` FROM `projektorok` WHERE `visszahozta` is not null ORDER BY `visszahozta` LIMIT 1
